18298975db007c19328fb572f7d43c0eb1d6673e
Lab2-13 <Lab2-13@LAB2-13>
2025-05-06 18:33:35 -0600

    Agregar archivos de proyecto.

---
+++ Form1.Designer.cs
@@ -0,0 +1,88 @@
+﻿namespace El_que_sea_1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dgDATOS = new System.Windows.Forms.DataGridView();
+            this.btnMostrar = new System.Windows.Forms.Button();
+            this.btnExportar = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dgDATOS)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dgDATOS
+            // 
+            this.dgDATOS.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgDATOS.Location = new System.Drawing.Point(68, 37);
+            this.dgDATOS.Name = "dgDATOS";
+            this.dgDATOS.Size = new System.Drawing.Size(501, 203);
+            this.dgDATOS.TabIndex = 0;
+            // 
+            // btnMostrar
+            // 
+            this.btnMostrar.Location = new System.Drawing.Point(201, 298);
+            this.btnMostrar.Name = "btnMostrar";
+            this.btnMostrar.Size = new System.Drawing.Size(67, 30);
+            this.btnMostrar.TabIndex = 1;
+            this.btnMostrar.Text = "Mostrar";
+            this.btnMostrar.UseVisualStyleBackColor = true;
+            this.btnMostrar.Click += new System.EventHandler(this.btnMostrar_Click);
+            // 
+            // btnExportar
+            // 
+            this.btnExportar.Location = new System.Drawing.Point(349, 298);
+            this.btnExportar.Name = "btnExportar";
+            this.btnExportar.Size = new System.Drawing.Size(67, 30);
+            this.btnExportar.TabIndex = 2;
+            this.btnExportar.Text = "Exportar";
+            this.btnExportar.UseVisualStyleBackColor = true;
+            this.btnExportar.Click += new System.EventHandler(this.btnExportar_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnExportar);
+            this.Controls.Add(this.btnMostrar);
+            this.Controls.Add(this.dgDATOS);
+            this.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))), ((int)(((byte)(4)))));
+            this.Name = "Form1";
+            this.Text = "kaka c";
+            ((System.ComponentModel.ISupportInitialize)(this.dgDATOS)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dgDATOS;
+        private System.Windows.Forms.Button btnMostrar;
+        private System.Windows.Forms.Button btnExportar;
+    }
+}
+
---
+++ aCCIONES.cs
@@ -0,0 +1,69 @@
+﻿using ClosedXML.Excel;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace El_que_sea_1
+{
+    internal class aCCIONES
+    {
+        private List<Alumno> alumnoList = new List<Alumno>
+        {
+            new Alumno ("Cindy",20,"LADD",112816,DateTime.Today),
+            new Alumno ("Rebeca",20,"LADD",112869,DateTime.Today),
+            new Alumno ("Maya",19,"LADD",111111,DateTime.Today),
+            new Alumno ("angela",20,"LADD",123456, DateTime.Today)
+        };
+        public List <Alumno> Mostrar()
+        {
+            return alumnoList;
+        }
+       
+
+        public bool ExportaraExcel()
+        {
+            try
+            {
+                var rutaEscritorio = Environment.GetFolderPath(Environment.SpecialFolder.Desktop);
+                var rutaArchivo = Path.Combine(rutaEscritorio, "Alumnos.xlsx");
+
+                using (var workbook = new XLWorkbook())
+                {
+                    var worksheet = workbook.Worksheets.Add("Alumnos");
+
+                    // Encabezados
+                    worksheet.Cell(1, 1).Value = "Nombre";
+                    worksheet.Cell(1, 2).Value = "Edad";
+                    worksheet.Cell(1, 3).Value = "Carrera";
+                    worksheet.Cell(1, 4).Value = "Matricula";
+                    worksheet.Cell(1, 5).Value = "Fecha de Nacimiento";
+
+                    // Datos
+                    for (int i = 0; i < alumnoList.Count; i++)
+                    {
+                        var alumno = alumnoList[i];
+                        worksheet.Cell(i + 2, 1).Value = alumno.Nombre;
+                        worksheet.Cell(i + 2, 2).Value = alumno.Edad;
+                        worksheet.Cell(i + 2, 3).Value = alumno.Carrera;
+                        worksheet.Cell(i + 2, 4).Value = alumno.Matricula;
+                        worksheet.Cell(i + 2, 5).Value = alumno.Fechanacimiento.ToShortDateString();
+                    }
+
+                    workbook.SaveAs(rutaArchivo);
+                }
+
+                return true;
+            }
+            catch (Exception)
+            {
+                return false;
+            }
+        }
+
+
+}
+}
