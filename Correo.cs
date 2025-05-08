349730b51ff7bb41835710492523f41ac9853e6d
Lab2-13 <Lab2-13@LAB2-13>
2025-05-08 17:48:41 -0600

    cmelck

---
+++ Correo.cs
@@ -0,0 +1,24 @@
+﻿using DocumentFormat.OpenXml.Drawing;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace El_que_sea_1
+{
+    internal class Correo
+    {
+        public void EnviarCorreo(string error)
+        {
+            try
+            {
+
+            }
+            catch (Exception )
+            {
+                throw;
+            }
+        }
+    }
+}
